8828040f0edf51631ab5e60eb13ed3c99a377bdc
ryo <fry3292@gmail.com>
2023-06-01 22:52:50 +0900

    🎨InputTextの送信ボタン

--- src/ui/input-text.tsx
+++ src/ui/input-text.tsx
@@ -27,7 +27,7 @@ const InputText = () => {
 
 const SendButton = ({ disabled = false }: { disabled: boolean }) => {
   return (
-    <button className="absolute p-2 rounded-md bottom-[6px] md:bottom-3 md:p-2 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 disabled:text-gray-400 enabled:bg-brand-purple text-white transition-colors disabled:opacity-40 enabled:bg-fuchsia-300" disabled={disabled} >
+    <button className="absolute p-2 rounded-md bottom-[6px] md:bottom-3 md:p-2 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 disabled:text-gray-400 text-white transition-colors disabled:opacity-40 enabled:bg-green-400" disabled={disabled} >
       <span>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="none" className="h-4 w-4" strokeWidth="2">
           <path d="M.5 1.163A1 1 0 0 1 1.97.28l12.868 6.837a1 1 0 0 1 0 1.766L1.969 15.72A1 1 0 0 1 .5 14.836V10.33a1 1 0 0 1 .816-.983L8.5 8 1.316 6.653A1 1 0 0 1 .5 5.67V1.163Z" fill="currentColor">
